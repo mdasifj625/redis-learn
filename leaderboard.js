ec0341b37910757fa978437b78bfa3b511f20681
mdasifj625 <mdasifj625@gmail.com>
2025-01-13 01:48:38 +0530

    created leaderboard with redis shorted set

---
+++ leaderboard.js
@@ -0,0 +1,27 @@
+import express, { request, response } from 'express';
+import redisClient from './redis.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/health', (req, res, _) => {
+	res.send({ message: 'Hello from rate limmit test' });
+});
+
+app.get('/api/user/:id/:score', async (req, res) => {
+	const { id, score } = req.params;
+	const leaderboardKey = `leaderboard`;
+	await redisClient.ZADD(leaderboardKey, {
+		score,
+		value: id,
+	});
+
+	const leaderboardList = await redisClient.ZRANGE(leaderboardKey, 0, 500000);
+	console.log({ id, score, leaderboardList });
+
+	res.send(leaderboardList);
+});
+
+app.listen(3000, () => console.log('Server is up on 3000 port'));
